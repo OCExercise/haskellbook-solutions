5e25ba51af7787040e7a8eef4fd931a81c737cbb
Prez Cannady <revprez@opencorrelate.org>
2017-08-29 19:52:36 -0400

    Removing scratch from chapter02

--- chapters/chapter02/scratch/FunctionWithLet.hs
+++
@@ -1,5 +0,0 @@
--- FunctionWithLet.hs
-module FunctionWithLet where
-
-printInc2 n = let plusTwo = n + 2
-              in print plusTwo
--- chapters/chapter02/scratch/FunctionWithWhere.hs
+++
@@ -1,5 +0,0 @@
--- FunctionWithWhere.hs
-module FunctionWithWhere where
-
-printInc n = print plusTwo
-             where plusTwo = n + 2
